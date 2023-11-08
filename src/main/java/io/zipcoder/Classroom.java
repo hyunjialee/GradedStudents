8c521a2f21f7127264d494e618170b25554c3c8f
hyunjialee <hyjialee@gmail.com>
2023-11-08 10:31:27 -0500

    starting on lab

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,4 +1,15 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+
 public class Classroom {
+    String firstName;
+    String lastName;
+    ArrayList<Double> examScores;
+
+    public Classroom(String firstName, String lastName, ArrayList<Double> examScores) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.examScores = examScores;
+    }
 }
--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,36 @@
 package io.zipcoder;
 
 public class Student {
+    String firstName;
+    String lastName;
+    Double[] testScore;
+
+    public Student (String firstName, String lastName, Double[] testScore) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.testScore = testScore;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Double[] getTestScore() {
+        return testScore;
+    }
+
+    //Additionally, define a getter, getNumberOfExamsTaken(),
+    // which returns the total number of exams taken by this student.
 }
