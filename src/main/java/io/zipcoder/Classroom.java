fc118b1e940b9649c5d53af284ee9c6e065f7de1
hyunjialee <hyjialee@gmail.com>
2023-11-08 22:45:57 -0500

    classroom methods

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,11 +1,47 @@
 package io.zipcoder;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Classroom {
     Student[] students;
+    int maxNumberOfStudents;
+
 
     public Classroom(Student[] students) {
         this.students = students;
     }
+
+    public Classroom(int maxNumberOfStudents){
+        this.students = new Student[maxNumberOfStudents];
+
+    }
+    public Classroom(){
+        this.students = new Student[30];
+    }
+
+    public Student[] getStudents(){
+        return this.students;
+    }
+    public Double getAverageExamScore(){
+        Double sum = 0.0;
+        for (Student x : this.students){
+            sum += x.getAverageExamScore();
+        }
+        return sum / this.students.length;
+    }
+    public void addStudent(Student student){
+
+    }
+    public removeStudent(String firstName, String lastName) {
+
+    }
+    public getStudentByScore(){
+
+    }
+    public getGradeBook(){
+
+    }
+
+
 }
