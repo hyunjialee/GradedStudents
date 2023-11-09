54058e7074bcc336ada8c4e23958fb1c7bf812bb
hyunjialee <hyjialee@gmail.com>
2023-11-08 23:05:22 -0500

    method update

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -12,27 +12,54 @@ public class Classroom {
         this.students = students;
     }
 
-    public Classroom(int maxNumberOfStudents){
+    public Classroom(int maxNumberOfStudents) {
         this.students = new Student[maxNumberOfStudents];
 
     }
-    public Classroom(){
+
+    public Classroom() {
         this.students = new Student[30];
     }
 
-    public Student[] getStudents(){
+    public Student[] getStudents() {
         return this.students;
     }
-    public Double getAverageExamScore(){
+
+    public Double getAverageExamScore() {
         Double sum = 0.0;
-        for (Student x : this.students){
+        for (Student x : this.students) {
             sum += x.getAverageExamScore();
         }
         return sum / this.students.length;
     }
-    public void addStudent(Student student){
 
+    public void addStudent(Student student) {
+        // Need to add one to the array------------------------------------------
+//        Student[] addStudent;
+//        if (this.students != null && this.students.length > 1) {
+//            addStudent = new Student[this.students.length + 1];
+//            for (int i = 0; i < this.students.length; i++) {
+//                addStudent[i] = this.students[i];
+//            }
+//            addStudent[addStudent.length - 1] = student;
+//        } else {
+//            addStudent = new Student[]{student};
+//        }
+//        this.students = addStudent; ------------------------------------------
+
+        // THE ABOVE DOES NOT WORK, When an array is created it takes null values into array if they are not used
+        // Example an array of 10 students are made and only 5 students are take into the first 5 slots
+        // the other five have null
+        //  This is only pertaining to OBJECT ARRAYS.
+
+        for (int i = 0 ; i <this.students.length; i++){
+            if(this.students[i] == null){
+                this.students[i] = student;
+                break;
+            }
+        }
     }
+
     public removeStudent(String firstName, String lastName) {
 
     }
@@ -42,6 +69,4 @@ public class Classroom {
     public getGradeBook(){
 
     }
-
-
 }
